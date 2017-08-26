5bd5efaea48dfc80fad893e1d695ce1a5bc2ae48
Netflux <netfluxer@gmail.com>
2017-08-26 12:18:31 +0800

    Add route for current ticket, Update GET route for all tickets
    * The GET route for all tickets now takes into account a "lastID" query.
    This is used in conjunction with the Ticket Logs to only fetch tickets
    that have been modified since the last fetch.

--- src/server/database/db.js
+++ src/server/database/db.js
@@ -3,7 +3,8 @@ import path from 'path'
 
 const db = new Sequelize('QSDatabase', null, null, {
 	dialect: 'sqlite',
-	storage: path.join(__dirname, '../../../dist/server/database/QSDatabase.sqlite')
+	storage: path.join(__dirname, '../../../dist/server/database/QSDatabase.sqlite'),
+	logging: false
 })
 
 export default db
--- src/server/routes.js
+++ src/server/routes.js
@@ -1,31 +1,95 @@
 import db from './database/db'
 import { TicketModel, TicketLogModel, UserModel } from './database/models'
 
-const ticketAttrs = { attributes: [ 'id', 'key', 'time_created', 'time_served', 'duration', 'cancelled' ] }
-const ticketLogAttrs = { attributes: [ 'id', 'ticket_id' ] }
+const ticketAttrs = [ 'id', 'key', 'time_created', 'time_served', 'duration', 'cancelled' ]
+const ticketLogAttrs = [ 'id', 'ticket_id' ]
 
 // Helper function to initialize all routes for the Express application
 const loadRoutes = app => {
 	app.get('/api/tickets', (req, res) => {
-		TicketModel.findAll(ticketAttrs).then(tickets => {
-			return res.json({ data: tickets })
-		})
-		.catch(err => {
-			console.error(err)
-			return res.sendStatus(500)
-		})
+		if (req.query.lastID) {
+			req.checkQuery('lastID', 'Invalid lastID').isInt()
+
+			req.getValidationResult().then(result => {
+				if (!result.isEmpty()) {
+					return res.status(400).json({
+						error: 'Invalid Parameter(s)',
+						fields: [ 'lastID' ]
+					})
+				}
+
+				req.sanitizeQuery('lastID').toInt()
+
+				TicketLogModel.findAll({
+					attributes: ticketLogAttrs,
+					where: {
+						id: { $gt: req.query.lastID }
+					},
+					order: [ [ 'id', 'ASC' ] ]
+				}).then(ticketLogs => {
+					return {
+						lastID: ticketLogs.length > 0 ? ticketLogs[ticketLogs.length - 1].id : null,
+						ticketIds: ticketLogs.map(log => log.ticket_id)
+							.filter((log, i, array) => array.indexOf(log) === i)
+					}
+
+				}).then(({ lastID, ticketIds }) => {
+					return TicketModel.findAll({
+						attributes: ticketAttrs,
+						where: {
+							id: { $in: ticketIds }
+						},
+						order: [ [ 'id', 'ASC' ] ]
+					}).then(tickets => {
+						return {
+							lastID,
+							data: tickets
+						}
+					})
+				}).then(payload => {
+					return res.json(payload)
+				}).catch(err => {
+					console.error(err)
+					return res.sendStatus(500)
+				})
+			})
+		} else {
+			TicketModel.findAll({
+				attributes: ticketAttrs,
+				order: [ [ 'id', 'ASC' ] ]
+			}).then(tickets => {
+				return TicketLogModel.findOne({
+					attributes: ticketLogAttrs,
+					order: [ [ 'id', 'DESC' ] ]
+				}).then(log => {
+					return res.json({
+						lastID: log ? log.id : -1,
+						data: tickets
+					})
+				})
+			}).catch(err => {
+				console.error(err)
+				return res.sendStatus(500)
+			})
+		}
 	})
 
 	app.post('/api/tickets', (req, res) => {
 		if (!req.body) {
-			return res.sendStatus(400)
+			return res.status(400).json({
+				error: 'Missing Parameter(s)',
+				fields: [ 'key' ]
+			})
 		}
 
 		req.checkBody('key', 'Invalid key').notEmpty().isString()
 
 		req.getValidationResult().then(result => {
 			if (!result.isEmpty()) {
-				return res.sendStatus(400)
+				return res.status(400).json({
+					error: 'Invalid Parameter(s)',
+					fields: [ 'key' ]
+				})
 			}
 
 			req.sanitizeBody('key').escape()
@@ -38,7 +102,7 @@ const loadRoutes = app => {
 					cancelled: false
 				}, { transaction: t }).then(ticket => {
 					// Return the new ticket data in the response
-					res.json({
+					res.status(201).json({
 						data: {
 							id: ticket.id,
 							key: ticket.key,
@@ -54,11 +118,27 @@ const loadRoutes = app => {
 						ticket_id: ticket.id
 					}, { transaction: t })
 				})
-			})
-			.catch(err => {
+			}).catch(err => {
 				console.error(err)
 				return res.sendStatus(500)
 			})
+		})
+	})
+
+	app.get('/api/tickets/current', (req, res) => {
+		TicketModel.findOne({
+			attributes: ticketAttrs,
+			where: {
+				time_served: null,
+				duration: null,
+				cancelled: false
+			},
+			order: [ [ 'id', 'ASC' ] ]
+		}).then(ticket => {
+			return res.json({ data: ticket })
+		}).catch(err => {
+			console.error(err)
+			return res.sendStatus(500)
 		})
 	})
 }
